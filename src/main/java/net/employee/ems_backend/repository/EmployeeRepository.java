02bae7262f15afb9ce73bfd517db539526f2f9e7
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-28 11:07:28 +0530

    Create EmployeeRepository.java

---
+++ src/main/java/net/employee/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package net.employee.ems_backend.repository;
+
+import net.employee.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    
+}
