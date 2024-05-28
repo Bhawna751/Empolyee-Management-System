da44f0a97548dd94a36cdc10de6247366c8b0a83
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-28 11:03:06 +0530

    created employee JPA entity

---
+++ src/main/java/net/employee/ems_backend/entity/Employee.java
@@ -0,0 +1,25 @@
+package net.employee.ems_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email_id",nullable = false,unique = true)
+    private String email;
+}
